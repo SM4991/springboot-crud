9dc973e6490b439f512805880c0de7d081ace6a3
SM4991 <mahawarsurabhi@gmail.com>
2022-12-01 12:01:00 +0000

    complete docker script for running applications

--- install.gitpod.sh
+++ install.gitpod.sh
@@ -1,27 +1,35 @@
-echo "Welcome to the installation script for Gitpod"
+echo "->Welcome to the installation script for Gitpod"
 
-echo "==> Ensuring .bashrc exists and is writable"
+echo "->Ensuring .bashrc exists and is writable"
 touch ~/.bashrc
 
 if [ -x "$(command -v docker)" ]; then
-    echo "Docker already available"
+    echo "->Docker already available"
     echo ""
 else
-    echo "Installing Docker"
+    echo "->Installing Docker"
     echo ""
     #  Install Docker
     curl -fsSL https://get.docker.com -o get-docker.sh
 fi
 
 if [ -x "$(command -v docker)" ]; then
-    echo "Docker Compose already available"
+    echo "->Docker Compose already available"
     echo ""
 else
-    echo "Installing docker-compose"
+    echo "->Installing docker-compose"
     echo ""
     sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 fi
 
-echo "Start postgresqldb"
+echo "->Create springboot-crud build"
+sudo apt-get update -y
+sudo apt-get install maven -y
+mvn clean install -DskipTests
+
+echo "->Start postgresqldb"
 docker-compose up -d postgresqldb
+
+echo "->Start springboot-crud application"
+docker-compose up -d springboot-crud
